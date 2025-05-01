680580d5a68a5e1038efbdc1a6e1bfb2c5c985d7
AGarciaJr <alejandrogarciacsi@gmail.com>
2025-05-01 13:16:19 -0500

    Fixed tag display in blog/index

---
+++ src/content/blog/hello-world2.md
@@ -0,0 +1,7 @@
+---
+title: "Hello, Astro!"
+date: 2025-05-01
+tags: [testing, tag2]
+---
+
+This is a testing file.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
